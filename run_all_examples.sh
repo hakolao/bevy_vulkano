3c122cebb95f202d25b387d97312c85338692329
hakolao <okkohakola@gmail.com>
2022-01-30 02:13:23 +0200

    Add run all examples script

---
+++ run_all_examples.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cargo run --example circle --features example_has_gui
+cargo run --example circle
+cargo run --example multi_window_gui --features example_has_gui
+cargo run --example multi_window_gui
